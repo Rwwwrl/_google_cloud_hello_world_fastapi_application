dd179f115a82cc69a1b39c9c87f43d142026e653
Aleksei Sosov <forgithublesh@yandex.ru>
2025-02-28 21:05:30 +0400

    added beanie to tests

---
+++ src/tests/pytest_beanie.py
@@ -0,0 +1,12 @@
+import pytest
+
+from src.models import User
+
+
+@pytest.mark.asyncio
+async def test_user():
+    inserted_user = await User(name="name").insert()
+
+    fetched_user = await User.find_one(User.name == "name")
+
+    assert inserted_user == fetched_user
