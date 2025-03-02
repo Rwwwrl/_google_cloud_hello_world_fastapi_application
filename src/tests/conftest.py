8eab38d0cd560421a366e54f643d48adf8578f29
Aleksei Sosov <forgithublesh@yandex.ru>
2025-03-02 13:32:53 +0400

    tmp

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,45 +1,39 @@
-import asyncio
-
-import pytest_asyncio
-from beanie import init_beanie
-from motor.motor_asyncio import AsyncIOMotorClient
-
-from src.main import app
-from src.models import User
 
 
-@pytest_asyncio.fixture(scope="session")
-async def mongodb_client():
-    """
-    Set up a test database for the session.
-    """
-    # Use a test database URI
-    test_db_uri = "mongodb://127.0.0.1:27017/?replicaSet=rs0"
-    client = AsyncIOMotorClient(test_db_uri)
-    client.get_io_loop = asyncio.get_event_loop
-    database_name = "test"
-    db = client.get_database(database_name)
-
-    await init_beanie(
-        database=db,
-        document_models=[
-            User,
-        ],
-    )
-
-    app.state.mongo_client = client
-
-    yield client
-
-    await client.drop_database(database_name)
 
 
-@pytest_asyncio.fixture(scope="function", autouse=True)
-async def clear_collections(mongodb_client):
-    """
-    Clear all collections before each test.
-    Don't clear pre-populated collections from migrations: capture_tasks, health_areas, biomarkers
-    """
-    for model in [User]:
-        await model.get_motor_collection().delete_many({})
-    yield
+# @pytest_asyncio.fixture(scope="session")
+# async def mongodb_client():
+#     """
+#     Set up a test database for the session.
+#     """
+#     # Use a test database URI
+#     test_db_uri = "mongodb://127.0.0.1:27017/?replicaSet=rs0"
+#     client = AsyncIOMotorClient(test_db_uri)
+#     client.get_io_loop = asyncio.get_event_loop
+#     database_name = "test"
+#     db = client.get_database(database_name)
+
+#     await init_beanie(
+#         database=db,
+#         document_models=[
+#             User,
+#         ],
+#     )
+
+#     app.state.mongo_client = client
+
+#     yield client
+
+#     await client.drop_database(database_name)
+
+
+# @pytest_asyncio.fixture(scope="function", autouse=True)
+# async def clear_collections(mongodb_client):
+#     """
+#     Clear all collections before each test.
+#     Don't clear pre-populated collections from migrations: capture_tasks, health_areas, biomarkers
+#     """
+#     for model in [User]:
+#         await model.get_motor_collection().delete_many({})
+#     yield
--- src/tests/pytest_beanie.py
+++ src/tests/pytest_beanie.py
@@ -1,12 +1,12 @@
-import pytest
+# import pytest
 
-from src.models import User
+# from src.models import User
 
 
-@pytest.mark.asyncio
-async def test_user():
-    inserted_user = await User(name="name").insert()
+# @pytest.mark.asyncio
+# async def test_user():
+#     inserted_user = await User(name="name").insert()
 
-    fetched_user = await User.find_one(User.name == "name")
+#     fetched_user = await User.find_one(User.name == "name")
 
-    assert inserted_user == fetched_user
+#     assert inserted_user == fetched_user
