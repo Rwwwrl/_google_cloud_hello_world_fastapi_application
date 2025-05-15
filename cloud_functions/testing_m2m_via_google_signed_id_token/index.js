e10c0c2e92c0d26c8e7f257f1eef91dd63cd2796
Aleksei Sosov <forgithublesh@yandex.ru>
2025-05-15 15:05:22 +0400

    [FHW-9] added BACKEND_API_HOST

--- cloud_functions/testing_m2m_via_google_signed_id_token/index.js
+++ cloud_functions/testing_m2m_via_google_signed_id_token/index.js
@@ -6,9 +6,9 @@ const keys = require("./keys.json");
 
 const AUDIENCE = process.env.AUDIENCE;
 
-functions.http("testingM2MViaGoogleSignedIdToken", async (req, res) => {
-    console.log(AUDIENCE);
+const BACKEND_API_HOST = process.env.BACKEND_API_HOST;
 
+functions.http("testingM2MViaGoogleSignedIdToken", async (req, res) => {
     const client = new JWT({
         email: keys.client_email,
         key: keys.private_key,
@@ -16,7 +16,20 @@ functions.http("testingM2MViaGoogleSignedIdToken", async (req, res) => {
 
     const googleSignedIdToken = await client.fetchIdToken(AUDIENCE);
 
-    console.log(googleSignedIdToken);
+    const url =
+        BACKEND_API_HOST +
+        "/api/hello-world/test_protected_with_google_JWT_token_endpoint/";
 
-    res.sendStatus(200);
+    try {
+        await fetch(url, {
+            method: "POST",
+            headers: {
+                Authorization: `Bearer ${googleSignedIdToken}`,
+            },
+        });
+        res.sendStatus(200);
+    } catch (error) {
+        console.error("Request failed:", error);
+        res.sendStatus(500);
+    }
 });
